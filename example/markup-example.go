e23e5316f63b1e43a4a805dacca5a586af035fe5
Ewan Drummond <45878916+edrumm@users.noreply.github.com>
2021-05-15 19:20:02 +0100

    Updated example

--- example/markup-example.go
+++ example/markup-example.go
@@ -3,10 +3,21 @@ package main
 import "github.com/edrumm/markup"
 
 func main() {
-	// Create new html page
-	// (* HTML)
+	/*
+		Create new html page
+		(*HTML)
+	*/
 	idx := markup.NewHtmlDoc("index.html", "A Markup Page")
+
+	/*
+		Create new stylesheet
+		(*Stylesheet)
+	*/
+	css := markup.NewStylesheet("style.css")
 
 	// Render page to html file
 	idx.Render()
+
+	// Render stylesheet to css file
+	css.Render()
 }
